896f7e1b65c12150b0db4e9f45b136452d8bcf45
James Spencer <jamess237@icloud.com>
2014-04-16 16:02:35 +1000

    document bloom filters

--- bloom.rb
+++ bloom.rb
@@ -1,10 +1,13 @@
 require 'bloomfilter-rb'
 
-# This class creates either a native or redis based
-# bloom filter.
-
+#== Bloom
+#
+#    redis = Redis.new # create a Redis connection
+#    bf = Bloom.new redis # returns a new Bloom instance
+#
 class Bloom
   def initialize(redis, options = {})
+    #merge options with defaults
     options = { 'native' => options['native'] || false,
                 'size' => options['size'] || 100,
                 'hashes' => options['hashes'] || 2,
@@ -14,22 +17,47 @@ class Bloom
                 'ttl' => options['ttl'] || 2,
                 'db' => redis }
 
+    # either use native implementation or redis backed
     @bf = BloomFilter::CountingRedis.new(options) unless options['native']
     @bf = BloomFilter::Native.new(options) if options['native']
   end
 
+  #== Insert
+  #
+  #    redis = Redis.new # create a Redis connection
+  #    bf = Bloom.new redis # returns a new Bloom instance
+  #    bf.insert("foo") # insert `foo` into the bloom filter
+  #
   def insert(obj)
     @bf.insert(obj)
   end
 
+  #== Delete
+  #
+  #    redis = Redis.new # create a Redis connection
+  #    bf = Bloom.new redis # returns a new Bloom instance
+  #    bf.delete("foo") # delete `foo` from the bloom filter
+  #
   def delete(obj)
     @bf.delete(obj)
   end
 
+  #== Includes
+  #
+  #    redis = Redis.new # create a Redis connection
+  #    bf = Bloom.new redis # returns a new Bloom instance
+  #    bf.includes?("foo") # does bloom filter include `foo`?
+  #
   def includes?(obj)
     @bf.include?(obj)
   end
 
+  #== Stats
+  #
+  #    redis = Redis.new # create a Redis connection
+  #    bf = Bloom.new redis # returns a new Bloom instance
+  #    bf.stats # prints stats
+  #
   def stats
     @bf.stats
   end
