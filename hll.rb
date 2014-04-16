8a4c8fbcefdc46323c5172894eaf5c909f057230
James Spencer <jamess237@icloud.com>
2014-04-16 16:13:36 +1000

    document hlld

--- hll.rb
+++ hll.rb
@@ -1,18 +1,36 @@
 require 'hyperloglog-redis'
 
-# This class creates a redis based HyperLogLog.
-
+#== HyperLog
+#
+#    redis = Redis.new # create a Redis connection
+#    hl = HyperLog.new redis # returns a new HyperLog instance
+#
 class HyperLog
   def initialize(redis)
     @counter = HyperLogLog::Counter.new(redis)
   end
 
+  #== Create
+  #
+  #    redis = Redis.new # create a Redis connection
+  #    hl = HyperLog.new redis # returns a new HyperLog instance
+  #    hl.create('cliche', ['foo', 'bar', 'baz', 'hello world']) # create a hyperlog array called `cliche` in redis, containing `foo`, `bar`, `baz` and `hello world`
+  #
   def create(name, array)
     array.each do |i|
       @counter.add(name, i)
     end
   end
 
+  #== Add
+  #
+  #    redis = Redis.new # create a Redis connection
+  #    hl = HyperLog.new redis # returns a new HyperLog instance
+  #    hl.create('cliche', ['foo', 'bar'])
+  #
+  #    hl.add('cliche', 'baz') # add `baz` to the hyperlog array `cliche`
+  #    hl.add('cliche', ['hello', 'world']) # add `hello` and `world` to the hyperlog array `cliche`
+  #
   def add(name, items)
     if items.is_a?(Array)
       items.each do |i|
@@ -23,18 +41,53 @@ class HyperLog
     end
   end
 
+  #== Count
+  #
+  #    redis = Redis.new # create a Redis connection
+  #    hl = HyperLog.new redis # returns a new HyperLog instance
+  #    hl.create('cliche', ['foo', 'bar'])
+  #
+  #    hl.count('cliche') # estimated count of items in the set
+  #
   def count(name)
     @counter.count(name)
   end
 
+  #== Union
+  #
+  #    redis = Redis.new # create a Redis connection
+  #    hl = HyperLog.new redis # returns a new HyperLog instance
+  #    hl.create('cliche', ['foo', 'bar'])
+  #    hl.create('eyy', ['bar', 'baz'])
+  #
+  #    hl.union(['cliche', 'eyy']) # intersect the sets `eyy` and `cliche`
+  #
   def union(sets)
     @counter.union(sets)
   end
 
+  #== Union
+  #
+  #    redis = Redis.new # create a Redis connection
+  #    hl = HyperLog.new redis # returns a new HyperLog instance
+  #    hl.create('cliche', ['foo', 'bar'])
+  #    hl.create('eyy', ['bar', 'baz'])
+  #
+  #    hl.union_store(['cliche', 'eyy'], 'sweet') # intersect the sets `eyy` and `cliche`, and store the result in the set `sweet`
+  #
   def union_store(sets, out)
     @counter.union_store(name, out)
   end
 
+  #== Intersect
+  #
+  #    redis = Redis.new # create a Redis connection
+  #    hl = HyperLog.new redis # returns a new HyperLog instance
+  #    hl.create('cliche', ['foo', 'bar'])
+  #    hl.create('eyy', ['bar', 'baz'])
+  #
+  #    hl.intersect(['cliche', 'eyy']) # intersect the sets `eyy` and `cliche`
+  #
   def intersect(sets)
     @counter.intersection(sets)
   end
